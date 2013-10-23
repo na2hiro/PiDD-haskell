bed8bf694ef05a04fe89f20afe63255fc2eb07f9
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-10-23 23:26:33 +0900

    better permu

--- permutation.hs
+++ permutation.hs
@@ -1,45 +1,39 @@
 type Var = Int
 -- 順列
-data Permutation = Permu [Var] | Trans (Var,Var) deriving(Show)
+newtype Seq = Seq { getSeq :: [Var] } deriving(Show,Eq)
 -- 互換
-instance Eq Permutation where
-  Permu a == Permu b = a==b
+newtype Trans = Trans { getTrans :: (Var,Var) } deriving(Show)
+instance Eq Trans where
   Trans a@(x1,y1) == Trans b@(x2,y2) = a==b || x1==y2 && x2==y1
-  _ == _ = False
 
--- 恒等順列
-pie :: Int -> Permutation
-pie num = Permu [0..num]
+instance Ord Trans where
+  Trans (x1,y1) `compare` Trans (x2,y2)
+    | x1 < y1 = LT
+    | x1 > y1 = GT
+    | x1==y1  = y2 `compare` y1 --ここが逆なので注意
 
--- 十分な長さの高等順列
-piem :: Permutation -> Permutation
-piem (Permu t) = pie $ length t
-piem (Trans (x,y)) = pie $ max x y
--- 積
-prod :: Permutation -> Permutation -> Permutation
-a@(Trans (x1,y1)) `prod` b@(Trans (x2,y2)) = let m = maximum [x1,y1,x2,y2]
-                                             in (pie m) `prod` a `prod` b
-p@(Permu xs) `prod` (Trans (x,y))
-  | x>=y = p `prod` Trans (y,x)
-  | x<y  = Permu $ chk xs (x,y) 0
-    where
-      chk :: [Var] -> (Var,Var) -> Int -> [Var]
-      chk (v:vs) p@(x,y) i
-              | i<x  = v:chk vs p (i+1)
-              | x==i = (xs!!y):chk vs p (i+1)
-              | i<y  = v:chk vs p (i+1)
-              | i==y = (xs!!x):vs
+-- 置換
+class Permutation a where
+  --表すのに必要な大きさ
+  size :: a -> Int
+  --次元
+  dim :: a -> Int
+  --互換の積に分解
+  factors :: a -> [Trans]
 
-t@(Trans pa) `prod` p@(Permu xs) = (piem t) `prod` t `prod` p
+instance Permutation Seq where
+  size = length  . getSeq
+  dim (Seq s) = dim' s 0
+        where
+          dim' :: [Var] -> Int -> Int
+          dim' [] _ = 0
+          dim' (x:xs) i
+            | x==i      = dim' xs (i+1)
+            | otherwise = max i $ dim' xs (i+1)
 
-p1@(Permu xs) `prod` p2@(Permu ys) = foldl prod p1 $ factors p2
-
--- 順列を互換の積に分解
-factors :: Permutation -> [Permutation]
-factors t@(Trans p)=[t]
-factors p@(Permu xs)=snd $ factors' (xs,[])
+  factors p@(Seq xs)=snd $ factors' (xs,[])
                   where
-                    factors' :: ([Var],[Permutation]) -> ([Var],[Permutation])
+                    factors' :: ([Var],[Trans]) -> ([Var],[Trans])
                     factors' (vs,rs)
                       | vs==[] =(vs,rs)
                       |otherwise = let lastpos=length vs - 1
@@ -56,22 +50,45 @@ factors p@(Permu xs)=snd $ factors' (xs,[])
                           set 0 v (x:xs) = v:xs
                           set i v (x:xs) = x:set (i-1) v xs
 
--- 順列の次元
-dim :: Permutation -> Int
-dim (Permu p) = dim' p 0
-        where
-          dim' :: [Var] -> Int -> Int
-          dim' [] _ = 0
-          dim' (x:xs) i
-            | x==i      = dim' xs (i+1)
-            | otherwise = max i $ dim' xs (i+1)
-dim tr@(Trans t) = dim $ (piem tr) `prod` tr
-   
-{-
+instance Permutation Trans where
+  size (Trans (x,y)) = max x y
+  dim = size
+  factors = return
+
+-- 恒等順列
+pie :: Int -> Seq
+pie num = Seq $ take num [0..]
+
+-- 十分な長さの高等順列
+piem :: (Permutation a)=> a -> Seq
+piem t = pie $ size t
+
+-- 互換の正規化（大きい方を前に）
+normalize :: Trans -> Trans
+normalize t@(Trans (x,y))
+  | x<y = Trans (y,x)
+  |otherwise = t
+
+-- 積
+prod :: (Permutation a)=> Seq -> a -> Seq
+prod p q = foldl apply p $ factors q
+
+-- 互換を1つ適用
+apply :: Seq -> Trans -> Seq
+p@(Seq xs) `apply` (Trans (x,y))
+  | x>=y = p `apply` Trans (y,x)
+  | x<y  = Seq $ chk xs (x,y) 0
+    where
+      chk :: [Var] -> (Var,Var) -> Int -> [Var]
+      chk (v:vs) p@(x,y) i
+              | i<x  = v:chk vs p (i+1)
+              | x==i = (xs!!y):chk vs p (i+1)
+              | i<y  = v:chk vs p (i+1)
+              | i==y = (xs!!x):vs
+
 main =do
        let e = pie 4
        print e
        print $ e `prod` Trans (0,2) `prod` Trans (2,1)
-       print $ dim $ Permu [1,2,0,4,3]
-       print $ factors $ Permu [1,2,0,4,3]
--}
+       print $ dim $ Seq [1,2,0,4,3]
+       print $ factors $ Seq [1,2,0,4,3]
