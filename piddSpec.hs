083b47516062ee207c7974d9ccf1614c87a98eef
na2hiro <a@81.la>
2013-10-27 21:39:53 +0900

    add test: figure 9

--- piddSpec.hs
+++ piddSpec.hs
@@ -6,7 +6,7 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "example" $ do
+    describe "Figure 7" $ do
       let v21 = trans2var$ Trans (2,1)
       let v32 = trans2var$ Trans (3,2)
       let a = Base
@@ -23,3 +23,24 @@ spec = do
       it "f=d*e" $ f `shouldBe` Node v32 (Node v21 Base Base) (Node v21 Base Base)
       let g = diff f e
       it "g=diff f e" $ g `shouldBe` Node v32 Empty (Node v21 Empty Base)
+
+    describe "Figure 9" $ do
+      let v21 = trans2var$ Trans (2,1)
+      let v31 = trans2var$ Trans (3,1)
+      let v32 = trans2var$ Trans (3,2)
+      let a21 = Node v21 Base Base
+      let a32 = Node v32 a21 a21
+      let a = Node v31 a32 a21
+      let v54 = trans2var$ Trans (5,4)
+      let v64 = trans2var$ Trans (6,4)
+      let v65 = trans2var$ Trans (6,5)
+      let b54 = Node v54 Base Base
+      let b65 = Node v65 b54 b54
+      let b = Node v64 b65 b54
+      let prodab = dprod a b
+
+      let p54 = Node v54 a a
+      let p65 = Node v65 p54 p54
+      let p = Node v64 p65 p54
+
+      it "a=Base" $ prodab `shouldBe` p
